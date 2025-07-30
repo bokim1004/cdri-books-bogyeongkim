3aeb7d6658dea8eeade8ede50849f3296ba9fa7a
chloekim <chloe@perfitt.io>
2025-07-30 18:53:42 +0900

    feat:header 컴포넌트 구현 완료

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,23 +1,21 @@
 function Header() {
   return (
-    <div className="relative flex items-center justify-between ">
-      <div className="text-textPrimary">CERTICOS BOOKS</div>
-      <nav className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  flex space-x-4">
-        <a href="/search" className="text-body2 hover:text-blue-600">
-          Search
+    <div className="relative flex items-center justify-between text-textPrimary ">
+      <div className=" text-title1 font-bold">CERTICOS BOOKS</div>
+      <nav className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  flex space-x-10">
+        <a
+          href="/search"
+          className="text-body1 font-medium hover:underline decoration-primary underline-offset-7"
+        >
+          도서 검색
         </a>
-        <a href="/about" className="text-body2 hover:text-primary">
-          About
+        <a
+          href="/favorites"
+          className="text-body1 font-medium hover:underline decoration-primary underline-offset-7"
+        >
+          내가 찜한 책
         </a>
       </nav>
-
-      {/* 디버깅용 */}
-      <div className="absolute top-16 left-0 p-2 bg-gray-100 text-xs space-y-1">
-        <div className="text-red-500">기본 빨강</div>
-        <div className="text-primary">커스텀 primary (파란색)</div>
-        <div className="text-textPrimary">커스텀 textPrimary (다크그레이)</div>
-        <div className="text-title1">title1 크기</div>
-      </div>
     </div>
   );
 }
--- src/pages/search-page.tsx
+++ src/pages/search-page.tsx
@@ -2,7 +2,7 @@ import Header from "../components/common/header";
 
 function SearchPage() {
   return (
-    <div className="flex flex-col items-base justify-baseline h-screen">
+    <div className="flex flex-col items-base justify-baseline h-screen font-display">
       <Header />
     </div>
   );
