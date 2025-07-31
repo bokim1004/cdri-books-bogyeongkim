40cd261a54227e87993a68dd4b3894ab9950d49b
chloekim <chloe@perfitt.io>
2025-07-31 11:33:26 +0900

    refactor:Header컴포넌트 리팩토링

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,21 +1,32 @@
-function Header() {
+type NavItems = {
+  label: string;
+  href: string;
+};
+
+type HeaderProps = {
+  logoText?: string;
+  navItems: NavItems[];
+};
+
+function Header({ logoText, navItems }: HeaderProps) {
   return (
     <div className="relative flex items-center justify-between text-textPrimary ">
-      <div className=" text-title1 font-bold">CERTICOS BOOKS</div>
-      <nav className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  flex space-x-10">
-        <a
-          href="/search"
-          className="text-body1 font-medium hover:underline decoration-primary underline-offset-7"
-        >
-          도서 검색
-        </a>
-        <a
-          href="/favorites"
-          className="text-body1 font-medium hover:underline decoration-primary underline-offset-7"
-        >
-          내가 찜한 책
-        </a>
-      </nav>
+      <div className=" text-title1 font-bold">{logoText}</div>
+      {navItems?.length > 0 ? (
+        <nav className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  flex space-x-10">
+          {navItems?.map((item) => {
+            return (
+              <a
+                key={item.href}
+                href={item.href}
+                className="text-body1 font-medium hover:underline decoration-primary underline-offset-7"
+              >
+                {item.label}
+              </a>
+            );
+          })}
+        </nav>
+      ) : null}
     </div>
   );
 }
--- src/pages/search-page.tsx
+++ src/pages/search-page.tsx
@@ -3,7 +3,13 @@ import Header from "../components/common/header";
 function SearchPage() {
   return (
     <div className="flex flex-col items-base justify-baseline h-screen font-display">
-      <Header />
+      <Header
+        logoText="CERTICOS"
+        navItems={[
+          { label: "도서 검색", href: "/search" },
+          { label: "내가 찜한 책", href: "/favorites" },
+        ]}
+      />
     </div>
   );
 }
